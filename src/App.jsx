5c8e045bffe25e934bf96e4a9e43520e5dce8fda
LaMar Fisher <lsfisher503@gmail.com>
2023-06-19 17:32:39 -0700

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,53 @@
-// Write your Color component here
+import React, { useState } from 'react';
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState('');
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color
+          color="red"
+          selectedColor={selectedColor}
+          onClickCallback={(color) => setSelectedColor(color)}
+        />
+        <Color
+          color="orange"
+          selectedColor={selectedColor}
+          onClickCallback={(color) => setSelectedColor(color)}
+        />
+        <Color
+          color="green"
+          selectedColor={selectedColor}
+          onClickCallback={(color) => setSelectedColor(color)}
+        />
+        <Color
+          color="blue"
+          selectedColor={selectedColor}
+          onClickCallback={(color) => setSelectedColor(color)}
+        />
+      </div>
     </div>
   );
 };
 
+const Color = ({ color, selectedColor, onClickCallback }) => {
+  const handleClick = () => {
+    onClickCallback(color);
+  };
+
+  return (
+    <div
+      className={`color ${color} ${selectedColor === color ? 'selected' : ''}`}
+      onClick={handleClick}
+    ></div>
+  );
+};
+
 export default App;
+
+
